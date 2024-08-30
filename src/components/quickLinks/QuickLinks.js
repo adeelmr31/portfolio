8df854eccf5c6e586d0ee3b603086d1adf0e8021
Nauman <inaumanmajeed@gmail.com>
2024-08-30 12:14:48 +0500

    chore: Update paths and import statements for lazy loading images

--- src/components/quickLinks/QuickLinks.js
+++ src/components/quickLinks/QuickLinks.js
@@ -2,10 +2,13 @@
 import React from "react";
 import { styled } from "@mui/material/styles";
 import FooterBg from "/static/footer-shape-2.svg";
+import Discord from "/static/discord.svg";
+import X from "/static/twitter.svg";
+import Linkedin from "/static/Linkedin.svg";
+import YouTube from "/static/youtube.svg";
 
 const Container = styled("div")(({ theme }) => ({
   marginTop: "100px",
-  height: "60vh",
   position: "relative",
   "& .bg__footer": {
     position: "absolute",
@@ -14,11 +17,76 @@ const Container = styled("div")(({ theme }) => ({
   },
 }));
 
+const ContentContainer = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexWrap: "wrap",
+  justifyContent: "space-between",
+  alignItems: "flex-start",
+  width: "80%",
+  margin: "0 auto",
+  padding: "5rem 0",
+}));
+
+const ContentMain = styled("div")(({ theme, flexBasis }) => ({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "flex-start",
+  alignItems: "flex-start",
+  flexBasis: flexBasis ? `calc(${flexBasis} - 1rem)` : "calc(25% - 1rem)",
+  gap: "2rem",
+}));
+
+const SocialLinks = styled("div")(({ theme }) => ({
+  display: "flex",
+  gap: "1rem",
+  "& .social__items": {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "40px",
+    height: "40px",
+    borderRadius: "50%",
+    backgroundColor: "#f5f8ff",
+    cursor: "pointer",
+    "&:hover": {
+      backgroundColor: "#3f7eff",
+      "& svg": {
+        fill: "#fff",
+      },
+    },
+  },
+}));
+
 const QuickLinks = () => {
   return (
     <Container>
       <FooterBg className="bg__footer" />
-      QuickLinks
+      <ContentContainer>
+        <ContentMain flexBasis={"30%"}>
+          <img src="/static/logo.svg" alt="logo" />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae
+            quam nec ante fringilla vel at erat convallis elit.
+          </p>
+          <SocialLinks className="social__links">
+            <div className="social__items">
+              <Discord />
+            </div>
+            <div className="social__items">
+              <X />
+            </div>
+            <div className="social__items">
+              <Linkedin />
+            </div>
+            <div className="social__items">
+              <YouTube />
+            </div>
+          </SocialLinks>
+        </ContentMain>
+        <ContentMain flexBasis={"15%"}>2</ContentMain>
+        <ContentMain flexBasis={"15%"}>3</ContentMain>
+        <ContentMain flexBasis={"30%"}>4</ContentMain>
+      </ContentContainer>
     </Container>
   );
 };
